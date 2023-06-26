88a405db1266ef842ed53e8b3fdd1f0041aedea7
Huzaifa Paghdiwala <huzaifa.paghdiwala.c@thalesdigital.io>
2023-06-27 01:42:32 +0800

    initial commit

--- test.test.ts
+++ test.test.ts
@@ -1,7 +1,12 @@
-import { expect } from 'jest';
+
+function compute(expression) {
+    return expression
+}
 
 describe('myFunction', () => {
     it('should return the correct value', () => {
+         expect(compute("1")).toBe("1")
+         expect(compute("100")).toBe("100")
 
     });
 
