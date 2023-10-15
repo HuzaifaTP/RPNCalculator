40c184385cab7d244b0166f728bda4c548f1d6aa
Huzaifa <huzaifa.paghdiwala.c@thalesdigital.io>
2023-10-15 13:08:00 +0800

    added some tests

--- test.test.ts
+++ test.test.ts
@@ -27,8 +27,13 @@ describe('rpnCalculator', () => {
          expect(compute('2 3 *')).toBe(6);
          expect(compute('8 4 /')).toBe(2);
     });
-    it('should handle unary operations', () => {
+    it('should handle unitary operations', () => {
         expect(compute('9 sqrt')).toBeCloseTo(3);
     });
+    it('should handle combination of expressions longer than 3')
+    expect(compute("5 4 sqrt 3 + -")).toBe(7);
+    expect(compute('2 6 sqrt 8 + -')).toBe(5);
+    expect(compute('1 8 3 + sqrt -')).toBe(6);
+    expect(compute('4 2 6 sqrt + -')).toBe(2);
 
 });
