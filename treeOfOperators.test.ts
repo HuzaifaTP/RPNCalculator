0dc789e2a0b3f83e0ff65bbf2649e9ea2b575087
uzi <uzi@uzi.com>
2023-12-19 17:14:33 +0800

    tree of operators interpretted via visitor pattern

--- treeOfOperators.test.ts
+++ treeOfOperators.test.ts
@@ -1,69 +1,139 @@
-import any = jasmine.any;
+interface ExpressionVisitor<T> {
+    visitLiteral(expression: Literal): T;
+    visitAddition(expression: Addition): T;
+    visitSubtraction(expression: Subtraction): T;
+    visitMultiplication(expression: Multiplication): T;
+    visitDivision(expression: Division): T;
+    visitSquareRoot(expression: SquareRoot): T;
+}
+
+//expression priority, parenthesis
+class RPNPrinter implements ExpressionVisitor<string> {
+    visitAddition(expression: Addition): string {
+        return `${expression.left.visit(this)} ${expression.right.visit(this)} +`;
+    }
+
+    visitDivision(expression: Division): string {
+        return undefined;
+    }
+
+    visitLiteral(expression: Literal): string {
+        return expression.value.toString();
+    }
+
+    visitMultiplication(expression: Multiplication): string {
+        return undefined;
+    }
+
+    visitSquareRoot(expression: SquareRoot): string {
+        return undefined;
+    }
+
+    visitSubtraction(expression: Subtraction): string {
+        return undefined;
+    }
+
+}
+
+class Evaluator implements ExpressionVisitor<number> {
+    visitAddition(expression: Addition): number {
+        return expression.left.visit(this) + expression.right.visit(this)
+    }
+
+    visitDivision(expression: Division): number {
+        return expression.left.visit(this) / expression.right.visit(this)
+    }
+
+    visitLiteral(expression: Literal): number {
+        return expression.value
+    }
+
+    visitMultiplication(expression: Multiplication): number {
+        return expression.left.visit(this) * expression.right.visit(this)
+    }
+
+    visitSquareRoot(expression: SquareRoot): number {
+        return Math.sqrt(expression.visit(this));
+    }
+
+    visitSubtraction(expression: Subtraction): number {
+        return expression.left.visit(this) - expression.right.visit(this)
+    }
+
+}
 
 interface Expression {
-    evaluate(): number;
+    visit<T>(visitor: ExpressionVisitor<T>): T;
 }
+
 class Literal implements Expression {
-    private value: number;
+    value: number;
+
     constructor(value: number) {
         this.value = value;
     }
-    evaluate(): number {
-        return this.value;
+
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitLiteral(this)
     }
 }
+
 class Addition implements Expression {
-    private left: Expression;
-    private right: Expression;
+    left: Expression;
+    right: Expression;
+
     constructor(left: Expression, right: Expression) {
         this.left = left;
         this.right = right;
     }
-    evaluate(): number {
-        return this.left.evaluate() + this.right.evaluate();
+
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitAddition(this)
     }
 }
-class Subtraction implements  Expression {
-    private left: Expression;
-    private right: Expression;
+
+class Subtraction implements Expression {
+    left: Expression;
+    right: Expression;
 
     constructor(left: Expression, right: Expression) {
-     this.left = left;
-     this.right = right;
-    }
-    evaluate(): number {
-        return this.left.evaluate() - this.right.evaluate()
-    }
-}
-class Multiplication implements Expression{
-    private left: Expression;
-    private right: Expression;
-
-    constructor( left: Expression, right: Expression) {
         this.left = left;
         this.right = right;
     }
-    evaluate():number{
-        return this.left.evaluate() * this.right.evaluate()
+
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitSubtraction(this)
+    }
+}
+
+class Multiplication implements Expression {
+    left: Expression;
+    right: Expression;
+
+    constructor(left: Expression, right: Expression) {
+        this.left = left;
+        this.right = right;
+    }
+
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitMultiplication(this)
     }
 }
 
 class Division implements Expression {
-    private left: Expression;
-    private right: Expression;
+    left: Expression;
+    right: Expression;
 
     constructor(left: Expression, right: Expression) {
         this.left = left;
         this.right = right;
     }
 
-    evaluate(): number {
-        if (this.right.evaluate() === 0) {
-            throw new Error("Division by zero");
-        }
-        return this.left.evaluate() / this.right.evaluate();
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitDivision(this)
     }
 }
+
 class SquareRoot implements Expression {
     private operand: Expression;
 
@@ -71,22 +141,19 @@ class SquareRoot implements Expression {
         this.operand = operand;
     }
 
-    evaluate(): number {
-        const result = this.operand.evaluate();
-        if (result < 0) {
-            throw new Error("Square root of negative number");
-        }
-        return Math.sqrt(result);
+    visit<T>(visitor: ExpressionVisitor<T>): T {
+        return visitor.visitSquareRoot(this)
     }
 }
+
 const binaryOperation = (operation: (left: Expression, right: Expression) => Expression) => {
-   return function process(stack: Expression[]){
-       const left = stack.pop()
-       const right = stack.pop()
-       stack.push(operation(left,right))
-   }
+    return function process(stack: Expression[]) {
+        const right = stack.pop()
+        const left = stack.pop()
+        stack.push(operation(left, right))
+    }
 }
-const unaryOperation = (operation: (operand: Expression)=>Expression) => {
+const unaryOperation = (operation: (operand: Expression) => Expression) => {
     return function process(stack: Expression[]) {
         const expression = stack.pop()
         stack.push(operation(operation(expression)))
@@ -101,43 +168,34 @@ const operatorDictionary = {
     'sqrt': unaryOperation((operand: Expression) => new SquareRoot(operand)),
 
 };
-const unaryOperations = {
-};
+const unaryOperations = {};
 
 
 class RPNCalculator {
-    static parseExpression(expression: string): Expression {
+    static parseExpression(expression: string): Expression | undefined {
         const stack: Expression[] = [];
         const tokens = expression.split(' ');
 
         for (const token of tokens) {
-            switch (token) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                    const right = stack.pop();
-                    const left = stack.pop();
-                    stack.push(binaryOperations[token](left, right));
-                    break;
-                case 'sqrt':
-                    const expressionFromStack = stack.pop();
-                    stack.push(unaryOperations[token](expressionFromStack))
-                    break;
-                default:
-                    stack.push(new Literal(parseFloat(token)));
+            if (token in operatorDictionary)
+                operatorDictionary[token](stack)
+            else {
+                stack.push(new Literal(parseFloat(token)));
             }
         }
-
         return stack.pop();
     }
 }
 
 describe('rpnCalculator', () => {
     it('should return the correct value for various operations in order', () => {
-        expect(RPNCalculator.parseExpression("3 4 +").evaluate()).toBe(7);
-        expect(RPNCalculator.parseExpression('10 5 -').evaluate()).toBe(5);
-        expect(RPNCalculator.parseExpression('2 3 *').evaluate()).toBe(6);
+        expect(RPNCalculator.parseExpression("3 4 +")?.visit(new Evaluator())).toBe(7);
+        expect(RPNCalculator.parseExpression('10 5 -')?.visit(new Evaluator())).toBe(5);
+        expect(RPNCalculator.parseExpression('2 3 *')?.visit(new Evaluator())).toBe(6);
+    });
+
+    it('should return the correct display for various operations ', () => {
+        expect(RPNCalculator.parseExpression("3 4 +")?.visit(new RPNPrinter())).toBe("3 4 +");
     });
 });
 
